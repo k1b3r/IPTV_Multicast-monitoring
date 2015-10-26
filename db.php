a2a329c4ca8360cd31934f8bc85705ca5aff20e1
k1b3r <leontiy.t@gmail.com>
2015-10-26 09:10:55 +0400

    Create db.php

---
+++ db.php
@@ -0,0 +1,18 @@
+ <?php
+$servername = "localhost";
+$username = "root";
+$password = "xxxxxxxxxx";
+$db = "diag_mcast";
+
+// Create connection
+$conn = mysql_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+ die('Could not connect: ' . mysql_error());
+} 
+$db_selected = mysql_select_db($db, $conn);
+if (!$db_selected) {
+    die ('Can\'t use db : ' . mysql_error());
+}
+?>
