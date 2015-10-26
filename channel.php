a8f56d30539a67e928afe567f6c82be2e51bbe57
Leon <leontiy.t@gmail.com>
2015-10-26 15:48:40 +0400

    fix screen view

--- channel.php
+++ channel.php
@@ -15,10 +15,10 @@ if($line[speed] == 00000){
 echo "$line[id] Chan <b><font color=\"yellow\">$line[name]</font></b> In use\or not works | <b>$time</b> min ago. <br>";
 }
 if($line[speed] < $sens and $line[speed] != 00000){
-echo "$line[id] Chan <b><font color=\"red\">$line[name]</font></b>\\$line[link] speed <b>$line[speed]</b> Kbit\s <a href=\"../screen/$id.jpg\" target=\"_blank\">screen</a>|<b>$time</b> min ago. <br>";
+echo "$line[id] Chan <b><font color=\"red\">$line[name]</font></b>\\$line[link] speed <b>$line[speed]</b> Kbit\s <a href=\"../screen/$line[id].jpg\" target=\"_blank\">screen</a>|<b>$time</b> min ago. <br>";
 }
 if($line[speed] != 00000 and $line[speed] > $sens){
-echo "$line[id] Chan <b><font color=\"green\">$line[name]</font></b>\\$line[link] speed <b>$line[speed]</b> Kbit\s <a href=\"../screen/$id.jpg\" target=\"_blank\">screen</a>|<b>$time</b> min ago.<br>";
+echo "$line[id] Chan <b><font color=\"green\">$line[name]</font></b>\\$line[link] speed <b>$line[speed]</b> Kbit\s <a href=\"../screen/$line[id].jpg\" target=\"_blank\">screen</a>|<b>$time</b> min ago.<br>";
  }
 }
 ?>
