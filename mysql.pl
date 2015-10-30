5d8149a70fb2cf129de6277629f4eb4f6dfb09b1
Leon <leontiy.t@gmail.com>
2015-10-30 13:26:05 +0400

    fork

--- mysql.pl
+++ mysql.pl
@@ -1,79 +1,97 @@
 #!/usr/bin/perl
-#use strict ;
-use warnings;
-use utf8;
-use Socket;
-use List::Util qw(sum);
-use IO::Socket;
-use IO::Socket::INET;
+use 5.014;
 use IO::Socket::Multicast;
-use Time::HiRes;
 use DBI;
 use Data::Dumper;
+use Time::HiRes;
 
-my $id;
-my $name;
-my $link;
 my $port = 5500;
-my $sock;
 my $time = 3;
-my $interface;
-my $mode = 0;
-my $file = "ch.txt";
+my $file;
 my $help;
+my $max_forks = 3;
+my $sock;
 
 my %config = do 'config.pl';
 
-
 use Getopt::Long;
-GetOptions("m"=> \$mode,
-"f"=> \$file,
-"h"=> \$help);
-sub usage(){
-    print "Multicast traffic checker\n";
-    print "==================================n";
-    print "Flags:n";
-    print "-m 0 is default  1 is writing to database\n";
-    print "-f file with data   data type is  name,ipaddr by default ch.txt\n";
-    print "-h Print this pagen";
-    exit();
+GetOptions(
+    "f" => \$file,
+    "h" => \$help
+);
+
+
+usage() if $help;
+
+my $dbh = DBI->connect(
+    "DBI:mysql:$config{db}:$config{dbserver}:$config{portsql}",
+    $config{user},
+    $config{password},
+    {
+        RaiseError             => 1,
+        AutoCommit             => 1,
+        mysql_multi_statements => 1,
+        mysql_init_command     => q{SET NAMES 'utf8'; SET CHARSET 'utf8'},
+        mysql_auto_reconnect   => 1,
+        AutoInactiveDestroy    => 1,
+    }
+) or die $DBI::errstr;
+
+
+if ( $file ) {
+    my $i = 0;
+    open( DATA, "./$file" ) or die "Couldn't open file $file, $!";
+    while (<DATA>) {
+        chomp; my ( $name, $link ) = split(',');
+        $dbh->do('INSERT  INTO chan (id,name,link) VALUES (?,?,?)',
+            undef, $i++, $name, $link);
+    }
+    exit;
 }
 
-if($help){
-    usage();
-    exit;
+########
+my ($count) = $dbh->selectrow_array('SELECT count(*) FROM chan');
+
+$SIG{CHLD} = 'IGNORE';
+my $pid;
+my $process_num = 0;
+for (1..$max_forks) {
+    $pid = fork();
+    #warn "$pid ::pid";
+    die "System is oveloaded" if ! defined $pid;
+    $process_num = $_;
+    last if ! $pid;
+} 
+exit(0) if $pid;
+$dbh = $dbh->clone;
+#my $limit  = $process_num == $max_forks ? '' : int($count/$max_forks);
+my $ost = $count % $max_forks;
+my $limit = int($count/$max_forks);
+my $offset = int($count/$max_forks) * ($process_num - 1);
+if ($process_num == $max_forks) {
+$offset+=$ost;
+$limit+=$ost;
 }
-elsif($mode==1 ){
-    my $i=0;
-    open(DATA, "./$file") or die "Couldn't open file ch.txt, $!";
-    while(<DATA>){
-        ($name,$link) = split(',');
-        my $dbh = DBI->connect("DBI:mysql:$config{db}:$config{dbserver}:$config{portsql}" ,$config{user},$config{password}) or
-        print "MySQL connection error $!";
-        $sth = $dbh->prepare("INSERT  INTO chan (id,name,link) VALUES ('$i','$name','$link')");# request
-        $sth->execute;
-        $i++;
-    }
-    $rc = $sth->finish;    # закрываем
-    exit;
-}
-if($mode==0){
-    my $dbh = DBI->connect("DBI:mysql:$config{db}:$config{dbserver}:$config{portsql}" ,$config{user},$config{password}) or
-    print "MySQL connection error $!";
-    $sth = $dbh->prepare("SELECT  id,name,link,interface FROM chan");# request
-    $sth->execute;
-    
-    while (($id,$name,$link,$interface) = $sth->fetchrow_array) {
-        #----------start from require id
-        if($id > 1000){
-            goto ERROR;
-            }else{
-            warn "$name";
-            #-----------------------------------
-            
-            $sock = IO::Socket::Multicast->new(Proto=>'udp',LocalAddr=> $link,LocalPort => $port,ReuseAddr => 1,ReusePort => 1,TimeOut => 7) or print "Error Open Socket"
-            ;
-            $sock->mcast_add($link,$interface) or print "Couldn't set group: $!\n";
+$dbh->{InactiveDestroy} = 1;
+my $sql = "SELECT id,name,link,interface FROM chan ORDER BY id LIMIT $limit OFFSET $offset";
+warn "$sql";
+my $sth = $dbh->prepare($sql);
+$sth->execute();
+while ( my ( $id, $name, $link, $interface ) = $sth->fetchrow_array ) {
+    warn "$id :: $name";
+
+     $sock = IO::Socket::Multicast->new(
+        Proto     => 'udp',
+        LocalAddr => $link,
+        LocalPort => $port,
+        ReuseAddr => 1,
+        ReusePort => 1,
+        TimeOut   => 7
+    ) or print "Error Open Socket: $!\n";
+    $sock->mcast_add( $link, $interface )
+        or die "Couldn't set group: $!\n";
+my $data;
+my $epoc;
             alarm(5);
             $sock->recv($data,4096) or print "Couldn't receive data $!\n";
             $SIG{ALRM} = sub {close($sock);};
@@ -86,20 +104,20 @@ if($mode==0){
                 }
             }
             close($sock);
-            my $size = length($data);
-            my $v = (($size/$time)*8)/10**3;
-            $v =sprintf("%.2f",$v);
-            #------------------------------------
-            #------------------------------------
-            $epoc = time();
-            $sth1 = $dbh->prepare("UPDATE  `chan` SET speed='$v',last_check='$epoc' WHERE id='$id' ");# request
-            $sth1->execute;
-            
-        }
-        ERROR:
-    }
-    $rc = $sth1->finish;
-    $rc = $sth->finish;    # закрываем
-    $rc = $dbh->disconnect;  # соединение
-    exit;
+    my $size = length($data);
+    # hope you'll not get division by 0 here
+    my $v = ( ( $size / $time ) * 8 ) / 10**3;
+    $v    = sprintf( "%.2f", $v );
+    $epoc = time();
+    $dbh->do("UPDATE  chan SET speed=?,last_check=? WHERE id=?",
+        undef, $v, $epoc, $id);
+}
+
+sub usage() {
+    print "Multicast traffic checker\n";
+    print "==================================n";
+    print "Flags:n";
+    print "-f file with data   data type is  name,ipaddr by default ch.txt\n";
+    print "-h Print this pagen";
+    exit();
 }
